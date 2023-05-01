79a496d01e799c7eab9e7040db2aee5600f7b1f4
Curioso1503 <estoestodo7@gmail.com>
2023-04-30 23:26:55 -0500

    actualizacion de nav,body,productos

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function(event) { 
+    // Obtener el elemento de la pantalla de carga
+    var pantallaCarga = document.querySelector(".pantalla-carga");
+  
+    // Ocultar la pantalla de carga después de 3 segundos
+    setTimeout(function() {
+      pantallaCarga.classList.add("oculto");
+    }, 1000);
+  });
