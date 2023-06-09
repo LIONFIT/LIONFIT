dbe75bf767fba3e05623bbf2b788a16b4507ff80
Curioso1503 <estoestodo7@gmail.com>
2023-06-09 10:30:45 -0500

    api maps

---
+++ wwwroot/js/script.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+    var coord = {lat:-12.070713 ,lng: -76.941267};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 15,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
